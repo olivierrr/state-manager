687c4441e2ffffac82f028c18ab2631736b30277
olivierrr <oliviersinbox@gmail.com>
2014-11-11 16:51:44 -0800

    common js & browser support

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
 
-/**
- * @constructor
- * each 'state' is expected to have a 'create' method. 'preload' and 'destroy' methods are optional
- */
-function StateManager (){
+(function (factory) {
+
+  if (typeof exports === 'object'){
+    module.exports = factory()
+  }else{
+    this.StateManager = factory()
+  }
+
+}(function () {
 
   /**
-   * @property {Hash#state}
+   * @constructor
+   * each 'state' is expected to have a 'create' method. 'preload' and 'destroy' methods are optional
    */
-  this.states = {}
+  function StateManager (){
+
+    /**
+     * @property {Hash#state}
+     */
+    this.states = {}
+
+    /**
+     * @property {Object#state}
+     */
+    this.currentState
+
+  }
 
   /**
-   * @property {Object#state}
+   * @method
+   * @param {String}
+   * @param {Object#state}
    */
-  this.currentState
+  StateManager.prototype.add = function(name, obj) {
 
-}
+    this.states[name] = obj
 
-/**
- * @method
- * @param {String}
- * @param {Object#state}
- */
-StateManager.prototype.add = function(name, obj) {
+    return this
 
-  this.states[name] = obj
+  }
 
-  return this
+  /**
+   * @method
+   * @param {String}
+   */
+  StateManager.prototype.go = function(name) {
 
-}
-
-/**
- * @method
- * @param {String}
- */
-StateManager.prototype.go = function(name) {
-
-  if(!this.states[name]){
-
-    console.warn(name + ' state does not exist.')
-    return
-
-  } else {
-
-    if(this.currentState && this.currentState.destroy){
-      this.currentState.destroy()
-    }
-
-    this.currentState = this.states[name]
-
-    if(this.currentState.preload){
-      var self = this
-      this.currentState.preload(function () {
-        self.currentState.create()
-      })
+    if(!this.states[name]){
+      console.warn(name + ' state does not exist.')
+      return
     } else {
-      this.currentState.create()
+
+      if(this.currentState && this.currentState.destroy){
+        this.currentState.destroy()
+      }
+
+      this.currentState = this.states[name]
+
+      if(this.currentState.preload){
+        var self = this
+        this.currentState.preload(function () {
+          self.currentState.create()
+        })
+      } else {
+        this.currentState.create()
+      }
+    }
+    return this
+  }
+
+  return {
+    create: function () {
+      return new StateManager()
     }
   }
 
-  return this
-
-}
-
-module.exports = StateManager
+}))
