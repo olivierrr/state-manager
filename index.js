3c0c15686b21e6a2b503abb8536688f8f2146280
olivierrr <oliviersinbox@gmail.com>
2014-11-11 16:20:45 -0800

    1.0

---
+++ index.js
@@ -0,0 +1,66 @@
+
+/**
+ * @constructor
+ * each 'state' is expected to have a 'create' method. 'preload' and 'destroy' methods are optional
+ */
+function StateManager (){
+
+  /**
+   * @property {Hash#state}
+   */
+  this.states = {}
+
+  /**
+   * @property {Object#state}
+   */
+  this.currentState
+
+}
+
+/**
+ * @method
+ * @param {String}
+ * @param {Object#state}
+ */
+StateManager.prototype.add = function(name, obj) {
+
+  this.states[name] = obj
+
+  return this
+
+}
+
+/**
+ * @method
+ * @param {String}
+ */
+StateManager.prototype.go = function(name) {
+
+  if(!this.states[name]){
+
+    console.warn(name + ' state does not exist.')
+    return
+
+  } else {
+
+    if(this.currentState && this.currentState.destroy){
+      this.currentState.destroy()
+    }
+
+    this.currentState = this.states[name]
+
+    if(this.currentState.preload){
+      var self = this
+      this.currentState.preload(function () {
+        self.currentState.create()
+      })
+    } else {
+      this.currentState.create()
+    }
+  }
+
+  return this
+
+}
+
+module.exports = StateManager
